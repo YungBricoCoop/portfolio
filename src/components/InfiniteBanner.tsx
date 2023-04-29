2c1ca154c1bb40eb991bd5b6de1de3e6a7caa417
Elwan Mayencourt <elwan.mayencourt@gmail.com>
2023-04-29 14:00:30 +0200

    💫 Improved the animations

--- src/components/InfiniteBanner.tsx
+++ src/components/InfiniteBanner.tsx
@@ -1,6 +1,9 @@
 // react
 import { useEffect } from 'react';
 
+// components
+import { VerticalTicker, HorizontalTicker } from 'react-infinite-ticker';
+
 // animations
 import { motion, useAnimation } from 'framer-motion';
 
@@ -8,60 +11,56 @@ import { motion, useAnimation } from 'framer-motion';
 interface InfiniteBannerProps {
     images: { src: string; label?: string; link?: string }[];
     speed?: number;
+    reverse?: boolean;
 }
 
 const InfiniteBanner: React.FC<InfiniteBannerProps> = ({
     images,
     speed = 1,
+    reverse = false,
 }) => {
-    const animationControls = useAnimation();
     const containerClass =
-        'w-64 h-24 flex items-center justify-center rounded-xl bg-white bg-opacity-0 hover:bg-opacity-5 transition-all duration-500';
+        'w-64 h-24 mx-2 flex items-center justify-center rounded-xl bg-white bg-opacity-0 hover:bg-opacity-5 transition-all duration-500';
 
-    useEffect(() => {
-        //FIXME: animation is not working properly and doesn't seem to be infinite
-        const animate = async () => {
-            await animationControls.start({ x: 0 });
-            await animationControls.start({
-                x: -(250 * (images.length - 1)),
-                transition: {
-                    duration: 10 / speed,
-                    ease: 'linear',
-                    repeat: Infinity,
-                },
-            });
-        };
-
-        animate();
-    }, [animationControls, images]);
+    const cardVariants = {
+        offscreen: {
+            opacity: 0,
+			y: 20,
+        },
+        onscreen: {
+            opacity: 1,
+			y: 0,
+        },
+    };
 
     return (
-        <div className='relative mx-auto overflow-x-hidden py-4'>
-            <motion.div
-                animate={animationControls}
-                className='flex items-center justify-center gap-2'
-                style={{ width: 250 * images.length * 2 }}
-            >
+        <motion.div
+            className='ticker my-8'
+            initial='offscreen'
+            whileInView='onscreen'
+            variants={cardVariants}
+            viewport={{ once: true, amount: 0.8 }}
+        >
+            <HorizontalTicker duration={20000 / speed} reverse={reverse}>
                 {images.map((image, index) => (
-                    <div key={index} className={containerClass}>
+                    <motion.div
+                        key={index}
+                        className={containerClass}
+                        onClick={() => {
+                            if (image.link) {
+                                window.open(image.link, '_blank');
+                            }
+                        }}
+                    >
                         <img
                             className='h-10'
                             src={image.src}
                             alt={image.label || ''}
                         />
-                    </div>
+                    </motion.div>
                 ))}
-                {images.map((image, index) => (
-                    <div key={index + images.length} className={containerClass}>
-                        <img
-                            className='h-10'
-                            src={image.src}
-                            alt={image.label || ''}
-                        />
-                    </div>
-                ))}
-            </motion.div>
-        </div>
+            </HorizontalTicker>
+        </motion.div>
     );
 };
 
