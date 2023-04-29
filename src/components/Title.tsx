657b512f263b405d3a3bbbbf4934fa281124bb35
Elwan Mayencourt <elwan.mayencourt@gmail.com>
2023-04-29 23:01:05 +0200

    💫 Display title on scroll

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,3 +1,6 @@
+// animation
+import { motion } from 'framer-motion';
+
 // interface
 interface TitleProps {
     title: string;
@@ -5,11 +8,28 @@ interface TitleProps {
 }
 
 const Title: React.FC<TitleProps> = ({ title, icon }) => {
+    const variants = {
+        offscreen: {
+            opacity: 0,
+            y: 20,
+        },
+        onscreen: {
+            opacity: 1,
+            y: 0,
+        },
+    };
+
     return (
-        <div className='flex w-full gap-2 justify-center items-center text-3xl font-bold text-center mt-12 mb-8 text-white'>
+        <motion.div
+            className='flex w-full gap-2 justify-center items-center text-3xl font-bold text-center mt-12 mb-8 text-white'
+            initial='offscreen'
+            whileInView='onscreen'
+            variants={variants}
+            viewport={{ once: true, amount: 0.8 }}
+        >
             {icon && <i className={`fas fa-${icon}`}></i>}
             {title}
-        </div>
+        </motion.div>
     );
 };
 
