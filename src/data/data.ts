eb914f8b8570c5a3f107a753a86660f5353ebeb1
Elwan Mayencourt <elwan.mayencourt@gmail.com>
2023-04-29 22:04:18 +0200

    ♻️ Refactored data

---
+++ src/data/data.ts
@@ -0,0 +1,74 @@
+export const techno = [
+	{ src: 'banner/react.svg', link: 'https://reactjs.org/' },
+	{
+		src: 'banner/typescript.png',
+		link: 'https://www.typescriptlang.org/',
+	},
+	{ src: 'banner/tailwind.png', link: 'https://tailwindcss.com/' },
+	{ src: 'banner/python.png', link: 'https://www.python.org/' },
+	{ src: 'banner/fastapi.png', link: 'https://fastapi.tiangolo.com/' },
+	{ src: 'banner/nodejs.svg', link: 'https://nodejs.org/en/' },
+];
+
+export const tools = [
+	{ src: 'banner/vscode.png', link: 'https://code.visualstudio.com/' },
+	{ src: 'banner/git.png', link: 'https://git-scm.com/' },
+	{ src: 'banner/postman.png', link: 'https://www.postman.com/' },
+	{ src: 'banner/termius.png', link: 'https://termius.com/' },
+	{ src: 'banner/copilot.png', link: 'https://copilot.github.com/' },
+	{ src: 'banner/openai.png', link: 'https://openai.com/' },
+	{ src: 'banner/figma.png', link: 'https://www.figma.com/' },
+	{ src: 'banner/photoshop.png', link: 'https://www.adobe.com/' },
+]
+
+export const projects = [
+	{
+		name: 'Portfolio V3',
+		description:
+			'The third iteration of my portfolio, i tried to make it as simple as possible, but still good looking.',
+		type: 'Web',
+		technologies: [
+			'React',
+			'Typescript',
+			'TailwindCSS',
+			'Framer Motion',
+			'Spline',
+			'ThreeJS',
+		],
+		link: 'https://elwan.ch/',
+		github: 'https://github.com/YungBricoCoop/portfolio',
+		image: 'gradients/gradient1.jpg',
+	},
+	{
+		name: 'ELMA',
+		description:
+			'ELMA is a private project, it is a complete solution for managing clients, invcoices, services. It is used to keep track of the work done for each client, generate invoices, and manage the payments.',
+		type: 'Fullstack',
+		technologies: [
+			'React',
+			'Typescript',
+			'TailwindCSS',
+			'MySQL',
+			'FastAPI',
+			'Python',
+			'Nginx',
+		],
+		image: 'gradients/gradient2.jpg',
+	},
+	{
+		name: 'Sophy Dashboard',
+		description:
+			'Sophy Dashboard is a private project, it is a dashboard used to manage job applications, and keep track of the progress and the relevancy of each application.',
+
+		type: 'Fullstack',
+		technologies: [
+			'React',
+			'TailwindCSS',
+			'FileMaker',
+			'Flask',
+			'Python',
+			'Nginx',
+		],
+		image: 'gradients/gradient3.jpg',
+	},
+]
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,36 +9,18 @@ import Page from '../components/Page';
 import Pointer from '../components/Pointer';
 import SplineC from '../components/SplineC';
 import Hero from '../components/Hero';
+import Title from '../components/Title';
 import PanoramaViewer from '../components/PanoramaViewer';
 import InfiniteBanner from '../components/InfiniteBanner';
+import Projects from '../components/Projects';
 
 // images
 import logo from '../assets/logo.png';
-import Projects from '../components/Projects';
-import Title from '../components/Title';
+
+// data
+import { techno, tools, projects } from '../data/data';
 
 const Home: React.FC = () => {
-    const techno = [
-        { src: 'banner/react.svg', link: 'https://reactjs.org/' },
-        {
-            src: 'banner/typescript.png',
-            link: 'https://www.typescriptlang.org/',
-        },
-        { src: 'banner/tailwind.png', link: 'https://tailwindcss.com/' },
-        { src: 'banner/python.png', link: 'https://www.python.org/' },
-        { src: 'banner/fastapi.png', link: 'https://fastapi.tiangolo.com/' },
-        { src: 'banner/nodejs.svg', link: 'https://nodejs.org/en/' },
-    ];
-    const tools = [
-        { src: 'banner/vscode.png', link: 'https://code.visualstudio.com/' },
-        { src: 'banner/git.png', link: 'https://git-scm.com/' },
-        { src: 'banner/postman.png', link: 'https://www.postman.com/' },
-        { src: 'banner/termius.png', link: 'https://termius.com/' },
-        { src: 'banner/copilot.png', link: 'https://copilot.github.com/' },
-        { src: 'banner/openai.png', link: 'https://openai.com/' },
-        { src: 'banner/figma.png', link: 'https://www.figma.com/' },
-        { src: 'banner/photoshop.png', link: 'https://www.adobe.com/' },
-    ];
     return (
         <Page>
             <Pointer latency={80} />
@@ -68,26 +50,8 @@ const Home: React.FC = () => {
             <InfiniteBanner images={techno} speed={1} />
             <InfiniteBanner images={tools} reverse speed={0.8} />
 
-			<Title title='Projects' icon='cubes' />
-            <Projects
-                projects={[
-                    {
-                        name: 'Portfolio',
-                        description:
-                            "Very long description that will be cut off at some point because it's too long and I don't want to write a lot of text for this project",
-                        type: 'Web',
-                        image: 'gradients/gradient1.jpg',
-                        technologies: ['React', 'Typescript', 'TailwindCSS'],
-                    },
-
-                    {
-                        name: 'My first project',
-                        description: 'This is a description',
-                        type: 'Web',
-                        image: 'gradients/gradient2.jpg',
-                    },
-                ]}
-            />
+            <Title title='Projects' icon='cubes' />
+            <Projects projects={projects} />
             <div className='w-full h-96 bg-transparent'></div>
         </Page>
     );
